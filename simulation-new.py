df8aa131053e7ff200ac9b4a4eb7a3f62f925ac3
Miggy <antonio_llamas@dlsu.edu.ph>
2018-03-13 23:04:47 +0800

    test

--- simulation-new.py
+++ simulation-new.py
@@ -1,3 +1,14 @@
 import struct
 
-#todo: everything
+def main():
+    deciInput = input("Input decimal mantissa: ")
+    deciExpInput = input("Input exponent (10^x): ")
+
+    deciInput = float(deciInput)
+    deciExpInput = float(deciExpInput)
+
+    product = deciInput * 10 ** deciExpInput
+
+    print(product)
+
+main()
