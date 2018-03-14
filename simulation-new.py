be8b2b697faf8638673510c7194006f05058d740
Miggy <antonio_llamas@dlsu.edu.ph>
2018-03-14 09:14:27 +0800

    extra case

--- simulation-new.py
+++ simulation-new.py
@@ -29,18 +29,19 @@ def main():
 	
     print('Binary:')
     if (inp < 1.0 and inp > 0.0):
-        print(hex(int(binstr, 2)))
-        print("00" + binstr + '\n')
+        m = binstr[:10]
+        e = binstr[10:]
+        print('0 0' + m + ' ' + e + '\n')
     elif (inp > 0):
         m = binstr[:11]
         e = binstr[11:]
         print('0 ' + m + ' ' + e + '\n')
-    if (inp < 0):
-        binstr = binstr[1:]
-        m = binstr[:11]
-        e = binstr[11:]
+    elif (inp < 0):
+        binstrTruncate = binstr[1:]
+        m = binstrTruncate[:11]
+        e = binstrTruncate[11:]
         print('1 ' + m + ' ' + e + '\n')
-    if (inp == 0):
+    elif (inp == 0):
         print("0x0000000000000000")
 
     print('Hexadecimal:')
